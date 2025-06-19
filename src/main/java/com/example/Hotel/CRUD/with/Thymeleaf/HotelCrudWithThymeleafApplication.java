248a692d864fee55095996df133e1268fb446d6b
Cemal <c.igrek63@hotmail.com>
2025-06-20 01:38:09 +0300

    Initial commit

---
+++ src/main/java/com/example/Hotel/CRUD/with/Thymeleaf/HotelCrudWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hotel.CRUD.with.Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCrudWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCrudWithThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hotel/CRUD/with/Thymeleaf/HotelCrudWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hotel.CRUD.with.Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCrudWithThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
